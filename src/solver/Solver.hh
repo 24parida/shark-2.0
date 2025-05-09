96a609a6059b9edbde7f24a6cc496f49da27caef
24parida <anubhavparida24@gmail.com>
2025-05-09 13:49:59 -0500

    init solver setup

--- src/solver/Solver.hh
+++ src/solver/Solver.hh
@@ -1,9 +1,20 @@
 #include "../hands/PreflopRangeManager.hh"
+#include "../hands/RiverRangeManager.hh"
 
 class ParallelDCFR {
-  PreflopRangeManager prm;
+  PreflopRangeManager m_prm;
+  RiverRangeManager m_rrm;
+
+  std::array<Card, 4> m_init_board;
+  int m_init_pot;
+  int m_in_position_player;
 
 public:
+  ParallelDCFR(const PreflopRangeManager prm,
+               const std::array<Card, 4> init_board, const int init_pot,
+               const int in_position_player)
+      : m_prm(prm), m_init_board(init_board), m_init_pot(init_pot),
+        m_in_position_player(in_position_player) {}
 };
 
 class CFRHeler {
